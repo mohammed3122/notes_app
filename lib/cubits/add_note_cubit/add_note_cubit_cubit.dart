73ab0bb24b8f2a7aac56c3cb127ca4b037a12ee9
Mohammad Awd <111813467+mohammed3122@users.noreply.github.com>
2025-11-22 09:28:50 +0400

    Hive.registerAdapter

--- lib/cubits/add_note_cubit/add_note_cubit_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit_cubit.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/models/note_model.dart';
 part 'add_note_cubit_state.dart';
 
 class AddNoteCubitCubit extends Cubit<AddNoteCubitState> {
   AddNoteCubitCubit() : super(AddNoteCubitInitial());
+
+  addNote(NoteModel note) {}
 }
