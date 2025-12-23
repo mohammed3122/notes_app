dcc2477a7e9cda893d973d78efe84f1971b7e15b
Mohammad Awd <111813467+mohammed3122@users.noreply.github.com>
2025-12-23 20:35:05 +0400

    edit

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/cubits/preview_note/preview_notes_cubit.dart';
-import 'package:notes_app/widgets/add_note_button.dart';
+import 'package:notes_app/widgets/add_note.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
 import 'package:notes_app/widgets/custom_button.dart';
 import 'package:notes_app/widgets/notes_list.dart';
